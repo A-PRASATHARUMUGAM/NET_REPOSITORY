fe3ef36c3dbc929aae9f9990571c8b122d23f4a5
Demo <Demo@GENXTHOFA-DEMO>
2025-08-12 10:27:45 +0530

    check update

--- Practice_.NET_Program/Program.cs
+++ Practice_.NET_Program/Program.cs
@@ -1,3 +1,12 @@
-﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿namespace Practice_.NET_Program
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a .NET 8.0 program using C# 12.0.");
+        }
+    }
+}
