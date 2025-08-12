a26bda7801d5d7329feefe82d031e8cfdfe684f4
PRASATH A <prasatharumugam040@gmail.com>
2025-08-12 12:34:20 +0530

    add structure of string methods

--- Practice_.NET_Program/String_Methos.cs
+++ Practice_.NET_Program/String_Methos.cs
@@ -1,1 +1,55 @@
 
+using System.Threading.Channels;
+
+namespace Practice_.NET_Program
+{
+                         //STRING AND ITS METHODS
+
+    /*
+     
+String Manipulation
+    Concatenation +
+    Formatting {0} {1}, a,b;
+    Interpolation $ {a} {b}
+    Verbatim @[/n and raw space]
+
+String Methods
+    Uppercase and Lowercase
+    SubString
+    Trim
+
+String Operations
+    IndexOf
+    IsNullOr WhiteSpace - null or blank
+    true else false
+       */
+
+
+    class String_Methods
+    {
+
+        // Static method to demonstrate a simple functionality
+        public static void data()
+        {
+
+
+
+
+            Console.WriteLine("Hello World!");
+            
+        }
+
+        // Instance method to demonstrate another functionality
+        // This method can be called on an instance of the class
+        // It is not static, so it requires an object of String_Methods to be created
+        // before it can be called.
+        // This is useful for methods that need to access instance-specific data or behavior.
+        public void helper() {
+
+            Console.WriteLine("Hi");
+         }
+
+
+    }
+}
+
