9c711055c22904ba8b8b5f0e9378b0a532b92471
PRASATH A <prasatharumugam040@gmail.com>
2025-08-13 11:50:46 +0530

    add the topic structure in collections in c sharp

---
+++ Practice_.NET_Program/Collections.cs
@@ -0,0 +1,33 @@
+
+namespace Practice_.NET_Program
+{
+     class Collections
+    {
+
+        //COLLECTIONS
+        //Array
+
+        //Types Of Collection
+
+        //Generic Collection - Limited to one type of object
+
+        //Non Generic Collection - Can store any type of object
+
+        //Generic Collection
+        //List,
+        //Dictionaries,
+        //Stack,
+        //Queue
+
+        //Non Generic Collection
+        //ArrayList,
+        //HashTable
+
+
+        public static void collect()
+        {
+
+            Console.WriteLine("Collect ");
+        }
+    }
+}
