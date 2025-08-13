b06f7f2731cf976749f4fbea9af1eaefb038f0e7
PRASATH A <prasatharumugam040@gmail.com>
2025-08-13 10:36:31 +0530

    create a new file to practice the looping in c sharp

---
+++ Practice_.NET_Program/Looping_Statement.cs
@@ -0,0 +1,11 @@
+
+namespace Practice_.NET_Program
+{
+     class Looping_Statement()
+    {
+       public static void Loop()
+        {
+            Console.WriteLine("check loop");
+        }
+    }
+}
