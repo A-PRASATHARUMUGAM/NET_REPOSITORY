e46245f07d8f0fa2f6dedbade6eff6cc5bb1b84e
PRASATH A <prasatharumugam040@gmail.com>
2025-08-13 11:42:54 +0530

    finish half loop

--- Practice_.NET_Program/Looping_Statement.cs
+++ Practice_.NET_Program/Looping_Statement.cs
@@ -1,11 +1,49 @@
 
+using System.Threading.Channels;
+
 namespace Practice_.NET_Program
 {
      class Looping_Statement()
     {
-       public static void Loop()
+
+
+                    //         LOOPS
+        // For
+        // While (check then run code block)
+        // Do while (run code block then it will check)
+        // Foreach (loop through array or list)
+        public static void Loop()
         {
-            Console.WriteLine("check loop");
+
+            // For
+
+            int i = 0;
+            for (i=0; i < 10; i++)
+            {
+                Console.WriteLine("For Loop "+i);
+            }
+
+            //While
+            int b = 0;
+            while (b < 10)
+            {
+                Console.WriteLine("While Loop "+b);
+                b++;
+            }
+
+            //Do While
+                int c = 0;
+            do
+            {
+                Console.WriteLine("Do While ",c);
+                c++;
+
+            } while (c < 10);
+
+
+            //Foreach
+
+
         }
     }
 }
