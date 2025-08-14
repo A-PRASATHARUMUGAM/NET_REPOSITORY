8d49b8a2386279bf9a4bd1ac270f97da7aa40118
PRASATH A <prasatharumugam040@gmail.com>
2025-08-14 14:13:27 +0530

    create a new folder to practice object oriented programming

---
+++ Practice_.NET_Program/Object_Oriented_Programming.cs
@@ -0,0 +1,11 @@
+
+namespace Practice_.NET_Program
+{
+     class Object_Oriented_Programming
+    {
+        public static void objpg()
+        {
+            Console.WriteLine("check object ");
+        }
+    }
+}
