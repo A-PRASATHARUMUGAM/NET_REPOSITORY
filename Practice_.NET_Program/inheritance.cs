d8c2e9153dfe57e197910a4e222d57ef722b95ae
PRASATH A <prasatharumugam040@gmail.com>
2025-08-18 17:51:34 +0530

    add structure in inheritance

---
+++ Practice_.NET_Program/inheritance.cs
@@ -0,0 +1,32 @@
+ //            INHERITANCE
+
+ //Inheritance - inherit properties and methods from one class to another.
+ //Base Class(parent) the class being inherited from
+ //Derived Class(child) - the class that inherits from another class
+
+         //Type of Inheritance
+ //Single Inheritance
+ //Multiple Inheritance(Only Achieved By Interface)
+ //Multilevel Inheritance
+ //Hierarchical Inheritance
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Program
+{
+    public class inheritance
+    {
+
+        public static void inheritancePg()
+        {
+            Console.WriteLine("check inherit");
+        }
+        
+    }
+}
+
