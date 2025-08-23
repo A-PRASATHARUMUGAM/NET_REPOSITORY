6ac262f6e1cd11c00d8185cb4ccbedda95f53ee5
PRASATH A <prasatharumugam040@gmail.com>
2025-08-23 12:50:07 +0530

    finsihed the method overriding

---
+++ Practice_.NET_Program/Polymorphism/Method_Overriding.cs
@@ -0,0 +1,39 @@
+using System;
+
+public class MainClass{
+    
+    public static void Main(string[] args){
+        
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        
+        dog.Speak();
+        cat.Speak();
+    }
+    
+    
+
+}
+public class Animal{
+    
+    public virtual void Speak(){
+        Console.WriteLine("Animal Makes Sound");
+    }
+    
+}
+
+public class Dog:Animal{
+    
+    public override void Speak(){
+        Console.WriteLine("Dog goes woof");
+    }
+
+}
+
+
+public class Cat:Animal{
+    public override void Speak(){
+        Console.WriteLine("Cat makes meow ");
+    }
+   
+}
