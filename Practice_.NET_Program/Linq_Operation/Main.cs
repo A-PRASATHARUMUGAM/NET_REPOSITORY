cc9dc8011cb1e8be9a9db7639a559c3ff4e7dd30
PRASATH A <prasatharumugam040@gmail.com>
2025-08-26 17:53:46 +0530

    add the main file

---
+++ Practice_.NET_Program/Linq_Operation/Main.cs
@@ -0,0 +1,21 @@
+using Practice_Program.Linq_Program;
+
+namespace Practice_Program
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            //Linq linq = new Linq();
+
+            //linq.checklist();
+
+            //Linq.checklist();
+
+            Linq_Operators linq_Operators = new Linq_Operators();
+
+            linq_Operators.OperEmployee();
+        }
+
+    }
+}
