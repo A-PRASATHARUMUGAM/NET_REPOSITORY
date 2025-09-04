ac244a89b5d8c18fcc399866bcbca0895a76019c
PRASATH A <prasatharumugam040@gmail.com>
2025-09-04 17:50:47 +0530

    Upload the web api source file

---
+++ Web_API_Project/World.API/Controllers/CountryController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using World.API.Data;
+using World.API.DTO.Country;
+using World.API.Models;
+
+namespace World.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+
+        public CountryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        //Fetch the Records 
+        [HttpGet]
+        public ActionResult<IEnumerable<Country>> GetALL()
+        {
+
+            var countries = _dbContext.Countries.ToList();
+            if(countries == null)
+            {
+                return NoContent();
+            }
+            return Ok(countries);
+        }
+
+
+        //Fetch the Specific Data using Id 
+        [HttpGet("{id:int}")]
+        public ActionResult<Country> GetById(int id)
+        {
+            var country= _dbContext.Countries.Find(id);
+
+            if(country == null)
+            {
+                return NoContent();
+            }
+            return Ok(country);
+        }
+
+
+        //create New Record
+        [HttpPost]
+        public ActionResult<CreateCountryDTO> Create([FromBody] CreateCountryDTO countryDTO)
+        {
+
+
+            Country country = new Country();
+
+            country.Name = countryDTO.Name;
+            country.ShortName = countryDTO.ShortName;
+            country.CountryCode = countryDTO.CountryCode;
+            _dbContext.Countries.Add(country);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        //Update the Data
+
+        [HttpPut]
+        public ActionResult<Country> Update([FromBody] Country country)
+        {
+            _dbContext.Countries.Update(country);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+        //Delete the Data
+
+        [HttpDelete("{id:int}")]
+        public ActionResult DeleteById(int id)
+        {
+            var country = _dbContext.Countries.Find(id);
+            _dbContext.Countries.Remove(country);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+
+      
+    }
+}
